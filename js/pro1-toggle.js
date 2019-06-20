b50bf15eef721618515d9dcdadc42225a03e3885
Sinyi <buns3366@gmail.com>
2019-06-20 13:08:14 +0800

    toggle其他收合
    toggle其他收合

--- js/pro1-toggle.js
+++ js/pro1-toggle.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
   ];
 
   //定義元素用的html模板，{{名稱}}代表要套入的地方
-  var tog_html = "<li id='{{item_tog_ids}}' class='item_tog' ><div id='{{title_tog_ids}}' class='title_tog' data-id='{{ids}}' open='0'>{{title}}<div id='{{btn_tog_ids}}' class='btn_tog_d'></div></div><div id='{{content_tog_ids}}' class='content_tog'>{{content}}</div></li>";
+  var tog_html = "<li class='item_tog' ><div id='{{title_tog_ids}}' class='title_tog' data-id='{{ids}}' open='0'>{{title}}<div id='{{btn_tog_ids}}' class='btn_tog_d'></div></div><div id='{{content_tog_ids}}' class='content_tog'>{{content}}</div></li>";
 
   //刪除並重新產生清單中所有項目
   function show() {
@@ -22,15 +22,13 @@ $(document).ready(function() {
 
     for(i = 0; i < tog.list.length; i++) {
       var item = tog.list[i];
-      var item_tog_id = "item_tog_" + i;
       var title_tog_id = "title_tog_" + i;
       var btn_tog_id = "btn_tog_" + i;
       var id = i;
       var content_tog_id = "content_tog_" + i;
 
       //取代模板位置成資料，replace(要取代的,取代成...)
-      var new_tog_html =  tog_html.replace("{{item_tog_ids}}", item_tog_id)
-                                  .replace("{{title_tog_ids}}", title_tog_id)
+      var new_tog_html =  tog_html.replace("{{title_tog_ids}}", title_tog_id)
                                   .replace("{{title}}", item.title)
                                   .replace("{{btn_tog_ids}}", btn_tog_id)
                                   .replace("{{ids}}", id)
@@ -42,7 +40,7 @@ $(document).ready(function() {
 
       //點選title_tog(各列標題區塊)，新增CSS展開，移除CSS收合
       $("#" + title_tog_id).click(function () {
-        // show();
+        show();
         var current_id = $(this).attr("data-id");
         $("#content_tog_"+current_id).toggleClass("content_tog_open");
         $("#btn_tog_"+current_id).toggleClass("btn_tog_u");
