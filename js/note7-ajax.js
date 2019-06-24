503c2d3d69a0d9bba3fcf52a99d9d6dce1d56b3e
Sinyi <buns3366@gmail.com>
2019-06-24 17:34:14 +0800

    新增筆記註解
    新增筆記註解

--- js/note7-ajax.js
+++ js/note7-ajax.js
@@ -2,11 +2,43 @@ var todo_url = "https://awiclass.monoame.com/api/command.php?type=get&name=todod
 var todolist_html = "<li class='{{class}}'>{{num}}.{{name}}<div class='date'>{{date}}</div></li>"
 
 //用ajax載入清單的物件內容
+
+/* =================================
+AJAX：Asynchronous JavaScript and XML
+-$.ajax(
+{    url: "伺服器端的網址",
+    type: "傳遞方式，可填入POST/GET",
+    data:  "傳送的參數"
+    success:  function(res){
+       載入進來的內容如何運用，res=回傳/取得的內容，預設為html，
+       可以到"datetyep"更改回傳的內容類型(html/ xml/ script/ json)。
+    }
+});
+這裡的例子 user_name: $(‘#user_name’).val()，
+傳送 $_GET[‘user_name’] 到 id_validate.php
+然後它的 value 是 $(‘#user_name’).val()
+ajax參數可參考： http://www.runoob.com/jquery/ajax-ajax.html
+-------------------------------------------
+if 條件式縮寫：
+- 如果條件式"a"成立，則變數b為1，不成立則變數b為2
+      var b="";
+      if (a== true){
+             b = "1";
+       }else {
+ b="2"
+}
+-- 效果等同於以下縮寫，「(條件)? "true的話"："false的話"」
+      var b = (a== true)? "1": "2";
+ ================================= */
+
 $.ajax(
   {
     url: todo_url,
     success: function (res) {
+
+      //JSON.parse()：解析 JSON 字串，能改變給定值和屬性、接著回傳解析值。
       todo_list = JSON.parse(res);
+
       for (i = 0; i < todo_list.length; i++) {
         var todoitem = todo_list[i];
         var doneclass = (todoitem.done == true) ? "item done" : "item";
--- js/note8-ajax.js
+++ js/note8-ajax.js
@@ -3,7 +3,6 @@ var menu_url = "https://awiclass.monoame.com/api/command.php?type=get&name=itemd
 var menu={};
 menu.name="胖嘟嘟水果店菜單";
 menu.date="2019-6-19";
-//商品清單的清單裡面是個陣列，塞商品物件們
 menu.list=[
 {name:"西瓜",price:"500"},
 {name:"草莓",price:"1200"},
@@ -13,6 +12,7 @@ menu.list=[
 
 ];
 
+//將變數內容替換為ajax內容
 $.ajax({
   url: menu_url,
   success: function (res) {
@@ -23,31 +23,24 @@ $.ajax({
 });
 
 
-//定義元素用的html模板，{{名稱}}代表要套入的地方
 var item_html="<li id='{{item_id}}' class='item'>{{num}} . {{name}}<div class='price'>{{price}}</div><div id='{{del_id}}' class='delbtn' data-id='{{id}}'>X</div></li>";
 
 var totle_html="<li id='totle_price' class='item_totle'>總計<div class='price_totle'>{{tol_price}}</div></li>";
 
 
-//刪除並重新產生清單中所有項目，包成一組function。
 function show() {
-//清空所有內容、總價歸0
 $("#menulist").html("");
 var new_totle_price = 0;
 
 for (i = 0; i < menu.list.length; i++) {
-
-  //品項，把每個項目做出來
   var item_id = "item_id_" + i;
   var new_name = menu.list[i].name;
   var new_price = menu.list[i].price;
   var new_del_id = "del_" + i;
   var id_del = i;
 
-  //已宣告過總價的變數，不需要再宣告一次，parseInt(...)表示將字串轉為整數數字；
   new_totle_price += parseInt(menu.list[i].price);
 
-  //品項，取代模板位置成資料，replace(要取代的,取代成...)
   var new_item_html =
     item_html.replace("{{item_id}}}", item_id)
       .replace("{{num}}", i + 1)
@@ -57,7 +50,6 @@ for (i = 0; i < menu.list.length; i++) {
       .replace("{{id}}", id_del)
 
   ;
-  //品項，用jquery操作，將新的HTML顯示出來
   $("#menulist").append(new_item_html);
 
   //刪除，點選後刪除指定id的陣列物件->重新渲染。陣列.splice(陣列第幾項,刪除多少個)
@@ -71,7 +63,6 @@ for (i = 0; i < menu.list.length; i++) {
 var new_totle_html =
   totle_html.replace("{{tol_price}}",new_totle_price);
 
-//總價，用jquery操作，將新的HTML顯示出來
 $("#menulist").append(new_totle_html);
 
 };
@@ -84,7 +75,6 @@ $(".addbtn").click(
 function () {
 menu.list.push(
   {
-    //使用val()存取輸入的值，val("..") 有給參數是設定
     name: $("#addmenu").val(),
     price: $("#addprice").val()
   }
